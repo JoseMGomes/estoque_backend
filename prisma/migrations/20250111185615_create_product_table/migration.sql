71926f6530dbda87771a30ff0d02b80626b490bb
José Lucas <gomessze03@gmail.com>
2025-01-11 16:17:00 -0300

    feat: create Product model and migration for MySQL database

---
+++ prisma/migrations/20250111185615_create_product_table/migration.sql
@@ -0,0 +1,18 @@
+
+ALTER TABLE [users]
+    ADD CONSTRAINT DF_users_createdAt DEFAULT GETDATE() FOR [createdAt];
+ALTER TABLE [users]
+    ADD CONSTRAINT DF_users_updatedAt DEFAULT GETDATE() FOR [updatedAt];
+
+
+CREATE TABLE [products] (
+    [id] INT IDENTITY(1,1) PRIMARY KEY, 
+    [name] NVARCHAR(191) NOT NULL,
+    [description] NVARCHAR(191) NOT NULL,
+    [price] DECIMAL(65, 30) NOT NULL,
+    [image] NVARCHAR(MAX) NOT NULL, 
+    [createdAt] DATETIME NOT NULL DEFAULT GETDATE(),
+    [updatedAt] DATETIME NOT NULL DEFAULT GETDATE(),
+
+    CONSTRAINT [products_name_key] UNIQUE ([name])
+);
---
+++ src/controllers/products.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+import { prismaClient } from "..";
+
+
+export const createProduct = async(req:Request, res:Response) => {
+    
+}
