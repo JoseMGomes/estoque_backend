47e8f62a6164fba3701f139e27ced54e8ebd33e8
José Lucas <gomessze03@gmail.com>
2025-01-12 03:06:59 -0300

    releasing colors

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,24 @@ import { PrismaClient } from "@prisma/client";
 import express, { Express, Request, Response } from "express";
 import rootRouter from "./routes";
 import { PORT } from "./secrets";
-
-import { SignUpSchema } from "./schema/userSchema";
+import cors from "cors"; 
 
 const app: Express = express();
+
+app.use(cors({
+  origin: '*'
+}));
+
 
 app.use(express.json());
 
 app.use("/api", rootRouter);
 
+
 export const prismaClient = new PrismaClient({
   log: ["query"],
 });
+
 
 app.listen(PORT, () => {
   console.log("App Funcionando!");
